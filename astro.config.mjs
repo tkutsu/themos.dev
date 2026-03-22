ee0afe524b80de9885c26c45687d82f86110d51c
Themis Kutsuris <themos.koutsouris@gmail.com>
2026-03-22 02:40:20 +0200

    removed wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://themos.dev',
-  adapter: cloudflare()
+  site: 'https://themos.dev'
 });
